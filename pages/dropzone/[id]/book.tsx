31d334562f4c69507a160deb71cda665b88ec400
Ping Cheng <aboudicheng@gmail.com>
2021-04-20 17:19:30 +0800

    feat(book): add reservation complete layout

--- pages/dropzone/[id]/book.tsx
+++ pages/dropzone/[id]/book.tsx
@@ -21,9 +21,12 @@ import {
 } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
 import { Elements } from '@stripe/react-stripe-js';
-import Input from 'components/Input';
+import dynamic from 'next/dynamic';
 import Button from 'components/Button';
 import Loader from 'components/Loader';
+import animationData from 'public/order/success.json';
+
+const Lottie = dynamic(() => import('react-lottie'));
 
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_API_KEY);
 
@@ -55,6 +58,7 @@ const BookDropzone: NextPage<Props> = ({ details }) => {
   const [isCardNumberComplete, setisCardNumberComplete] = useState(false);
   const [isExpiredDateComplete, setIsExpiredDateComplete] = useState(false);
   const [isCVCComplete, setIsCVCComplete] = useState(false);
+  const [isPaymentComplete, setIsPaymentComplete] = useState(true);
 
   const isCardInfoComplete = useMemo(() => {
     return isCardNumberComplete && isExpiredDateComplete && isCVCComplete;
@@ -151,97 +155,137 @@ const BookDropzone: NextPage<Props> = ({ details }) => {
         </div>
       </nav>
 
-      <div className="max-w-screen-lg my-0 mx-auto pt-20 pb-14">
-        <Link href={`/dropzone/${Router.query.id}`}>
-          <a
-            className="flex items-center text-orange cursor-pointer"
-            onClick={() => dispatch(clearOrder())}
-          >
-            <ArrowLeft className="mr-2" size={24} color="#FF8E6E" />
-            Back to Details
-          </a>
-        </Link>
-      </div>
+      {isPaymentComplete ? (
+        <div className="max-w-screen-lg my-0 mx-auto pt-20 pb-14">
+          <div className="w-full shadow-2xl rounded-lg py-10 flex items-center flex-col">
+            <Lottie
+              options={{
+                loop: true,
+                autoplay: true,
+                animationData,
+              }}
+              height={135}
+              width={135}
+              style={{ margin: 0 }}
+            />
 
-      <div className="max-w-screen-lg my-0 mx-auto flex">
-        <form onSubmit={onSubmit} className="w-3/5 mr-12">
-          <h1 className="text-3xl font-bold mb-10">Checkout</h1>
+            <h1 className="my-4 text-3xl">Reservation Successful!</h1>
+            <h3 className="mb-4">
+              Your order ID:{' '}
+              <a
+                className="text-orange"
+                href="/order/5f7bedb0ed5b"
+                target="_blank"
+                rel="noreferrer"
+              >
+                5f7bedb0ed5b
+              </a>
+            </h3>
+            <p className="max-w-xl mb-6 text-grey text-center">
+              Thank you for using Keepify. The host will confirm your order within a short while, so
+              please keep an eye on your email inbox!
+            </p>
 
-          <div className="pb-6">
-            <label className="text-dark text-sm mb-2">Credit Card Number</label>
-            <div className="mt-2">
-              <CardNumberElement
-                options={options}
-                onChange={(event) => setisCardNumberComplete(event.complete)}
-              />
-            </div>
-          </div>
-
-          <div className="pb-6 flex justify-between">
-            <div className="w-1/2 pr-2">
-              <label className="text-dark text-sm mb-2">Expiration Date</label>
-              <div className="mt-2">
-                <CardExpiryElement
-                  options={options}
-                  onChange={(event) => setIsExpiredDateComplete(event.complete)}
-                />
-              </div>
-            </div>
-            <div className="w-1/2 pl-2">
-              <label className="text-dark text-sm mb-2">CVC</label>
-              <div className="mt-2">
-                <CardCvcElement
-                  options={options}
-                  onChange={(event) => setIsCVCComplete(event.complete)}
-                />
-              </div>
-            </div>
-          </div>
-
-          <div>
-            <Button
-              type="submit"
-              className={`w-full mt-6 pointer-events-${isCardInfoComplete ? 'auto' : 'none'}`}
-            >
-              Place Order
-            </Button>
-          </div>
-        </form>
-        <div className="w-2/5">
-          <div className="shadow-2xl rounded-lg p-8">
-            <label className="tracking-wider text-lg block mb-4">Order Summary</label>
-            <div className="mb-2 flex justify-between items-center">
-              <label className="tracking-widest text-orange text-sm">Start</label>
-              <label className="tracking-widest text-sm">
-                {moment(orderInfo.startTime).format('YYYY/MM/DD H:mm A')}
-              </label>
-            </div>
-            <div className="mb-5 flex justify-between items-center">
-              <label className="tracking-widest text-orange text-sm">End</label>
-              <label className="tracking-widest text-sm">
-                {moment(orderInfo.endTime).format('YYYY/MM/DD H:mm A')}
-              </label>
-            </div>
-            <div className="mb-2 flex justify-between items-center">
-              <label className="tracking-widest text-sm">Price</label>
-              <label className="tracking-widest text-sm">${details.rate}/day</label>
-            </div>
-            <div className="mb-2 flex justify-between items-center">
-              <label className="tracking-widest text-sm">Number of Items</label>
-              <label className="tracking-widest text-sm">x{orderInfo.items}</label>
-            </div>
-
-            <hr className="w-full h-0.5 mt-4 bg-white opacity-20 mb-4" />
-
-            <div className="mb-2 flex justify-between items-center">
-              <label className="tracking-widest text-lg font-bold">Total</label>
-              <label className="tracking-widest text-lg font-bold">
-                ${(details.rate * orderInfo.items * totalDays).toFixed(2)}
-              </label>
-            </div>
+            <a href="/order/5f7bedb0ed5b" target="_blank" rel="noreferrer">
+              <Button>View Order</Button>
+            </a>
           </div>
         </div>
-      </div>
+      ) : (
+        <>
+          <div className="max-w-screen-lg my-0 mx-auto pt-20 pb-14">
+            <Link href={`/dropzone/${Router.query.id}`}>
+              <a
+                className="flex items-center text-orange cursor-pointer"
+                onClick={() => dispatch(clearOrder())}
+              >
+                <ArrowLeft className="mr-2" size={24} color="#FF8E6E" />
+                Back to Details
+              </a>
+            </Link>
+          </div>
+
+          <div className="max-w-screen-lg my-0 mx-auto flex">
+            <form onSubmit={onSubmit} className="w-3/5 mr-12">
+              <h1 className="text-3xl font-bold mb-10">Checkout</h1>
+
+              <div className="pb-6">
+                <label className="text-dark text-sm mb-2">Credit Card Number</label>
+                <div className="mt-2">
+                  <CardNumberElement
+                    options={options}
+                    onChange={(event) => setisCardNumberComplete(event.complete)}
+                  />
+                </div>
+              </div>
+
+              <div className="pb-6 flex justify-between">
+                <div className="w-1/2 pr-2">
+                  <label className="text-dark text-sm mb-2">Expiration Date</label>
+                  <div className="mt-2">
+                    <CardExpiryElement
+                      options={options}
+                      onChange={(event) => setIsExpiredDateComplete(event.complete)}
+                    />
+                  </div>
+                </div>
+                <div className="w-1/2 pl-2">
+                  <label className="text-dark text-sm mb-2">CVC</label>
+                  <div className="mt-2">
+                    <CardCvcElement
+                      options={options}
+                      onChange={(event) => setIsCVCComplete(event.complete)}
+                    />
+                  </div>
+                </div>
+              </div>
+
+              <div>
+                <Button
+                  type="submit"
+                  className={`w-full mt-6 pointer-events-${isCardInfoComplete ? 'auto' : 'none'}`}
+                >
+                  Place Order
+                </Button>
+              </div>
+            </form>
+            <div className="w-2/5">
+              <div className="shadow-2xl rounded-lg p-8">
+                <label className="tracking-wider text-lg block mb-4">Order Summary</label>
+                <div className="mb-2 flex justify-between items-center">
+                  <label className="tracking-widest text-orange text-sm">Start</label>
+                  <label className="tracking-widest text-sm">
+                    {moment(orderInfo.startTime).format('YYYY/MM/DD H:mm A')}
+                  </label>
+                </div>
+                <div className="mb-5 flex justify-between items-center">
+                  <label className="tracking-widest text-orange text-sm">End</label>
+                  <label className="tracking-widest text-sm">
+                    {moment(orderInfo.endTime).format('YYYY/MM/DD H:mm A')}
+                  </label>
+                </div>
+                <div className="mb-2 flex justify-between items-center">
+                  <label className="tracking-widest text-sm">Price</label>
+                  <label className="tracking-widest text-sm">${details.rate}/day</label>
+                </div>
+                <div className="mb-2 flex justify-between items-center">
+                  <label className="tracking-widest text-sm">Number of Items</label>
+                  <label className="tracking-widest text-sm">x{orderInfo.items}</label>
+                </div>
+
+                <hr className="w-full h-0.5 mt-4 bg-white opacity-20 mb-4" />
+
+                <div className="mb-2 flex justify-between items-center">
+                  <label className="tracking-widest text-lg font-bold">Total</label>
+                  <label className="tracking-widest text-lg font-bold">
+                    ${(details.rate * orderInfo.items * totalDays).toFixed(2)}
+                  </label>
+                </div>
+              </div>
+            </div>
+          </div>
+        </>
+      )}
     </article>
   );
 };
